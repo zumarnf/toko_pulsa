ea386f112ad1f2afd8c609f18d6ccbb25986b005
zumzum <kazonima@gmail.com>
2024-06-29 06:33:36 +0700

    firstcommt

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginPage from './pages/LoginPage';
+import './App.css';
+
+function App() {
+  return (
+    <LoginPage />
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginMenu from "../components/LoginMenu";
+
+function LoginPage() {
+  return <LoginMenu />;
+}
+
+export default LoginPage;
